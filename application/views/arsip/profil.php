241765c490c74d28de224cd50892666a765ff090
arumys78 <117717760+arumys78@users.noreply.github.com>
2022-11-08 16:38:24 +0700

    first

---
+++ application/views/arsip/profil.php
@@ -0,0 +1,15 @@
+<div class="m-3">
+    <h2>About</h2>
+    <div class="row">
+        <div class="col-3">
+            <img src="<?= base_url('assets/ktm.jpeg') ?>" class="img-fluid rounded img-cover-adjust" alt="">
+        </div>
+        <div class="col">
+            <h4>Aplikasi ini dibuat oleh</h4>
+            <p class="m-0">Nama : Arum Yulindasari</p>
+            <p class="m-0">NIM : 2031730056</p>
+            <p class="m-0">Tanggal : 07 November 2022</p>
+        </div>
+
+    </div>
+</div>
